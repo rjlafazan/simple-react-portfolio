00033c1cd55c986daaec69c3f2a99c11bd3a071c
Ryan Lafazan <rjlafazan@gmail.com>
2020-07-31 15:24:37 -0700

    update media bar to include resume file icon and link, update all hrefs to link to corresponding locations and resource, and include tooltips for each icon

--- src/components/MediaBar/index.js
+++ src/components/MediaBar/index.js
@@ -1,34 +1,38 @@
 import React from "react";
+import { resume2020 } from '../../assets/files';
 
 function MediaBar() {
+
   return (
     <section className="page-section" id="media-bar">
       <div className="container">
         <div className="row align-items-center">
           <div className="col-md-3">
-            <span className="copyright">
-              {/* Copyright &copy; Your Website 2019 */}
-            </span>
           </div>
           <div className="col-md-6 col-sm-">
             <ul className="list-inline social-buttons">
               <li className="list-inline-item">
-                <a href="tel:916-581-6050">
+                <a href="tel:916-581-6050" data-toggle="tooltip" data-placement="bottom" title="Mobile: 916-581-6050">
                   <i className="fa fa-phone"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="mailto:ryan@lafazan.org">
+                <a href="mailto:ryan@lafazan.org" data-toggle="tooltip" data-placement="bottom" title="Email: ryan@lafazan.org">
                   <i className="fa fa-envelope"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://github.com/rjlafazan">
+                <a href={resume2020} target="_blank" rel="noopener noreferrer" data-toggle="tooltip" data-placement="bottom" title="View Resume">
+                  <i className="fa fa-file-text"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://github.com/rjlafazan" data-toggle="tooltip" data-placement="bottom" title="Github: rjlafazan">
                   <i className="fa fa-github"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="https://www.linkedin.com/in/rjlafazan/">
+                <a href="https://www.linkedin.com/in/rjlafazan/" data-toggle="tooltip" data-placement="bottom" title="LinkedIn: rjlafazan">
                   <i className="fa fa-linkedin"></i>
                 </a>
               </li>
@@ -36,12 +40,6 @@ function MediaBar() {
           </div>
           <div className="col-md-3">
             <ul className="list-inline quicklinks">
-              {/* <li className="list-inline-item">
-                <a href="#something">Privacy Policy</a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">Terms of Use</a>
-              </li> */}
             </ul>
           </div>
         </div>
