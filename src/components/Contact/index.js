d253671a2e202cf3d21cc54d0beeade1154e4c52
Ryan Lafazan <rjlafazan@gmail.com>
2020-07-31 15:21:25 -0700

    add card to contact section component with contact information

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -11,10 +11,10 @@ function Contact() {
         </div>
         <div className="row">
           <div className="col-lg-12">
-            <div class="card">
-              <div class="portfolio-caption">
-                <h5 class="card-title">ryan@lafazan.org</h5>
-                <h5 class="card-title">916-581-6050</h5>
+            <div className="card">
+              <div className="portfolio-caption">
+                <h5 className="card-title">ryan@lafazan.org</h5>
+                <h5 className="card-title">916-581-6050</h5>
               </div>
             </div>
           </div>
